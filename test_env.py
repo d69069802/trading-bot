6f832e8b6613cee04c90886ebab4d169788634da
d69069802 <d69069802@gmail.com>
2026-04-28 14:15:45 +1000

    add live file

--- test_env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("ALPACA_KEY:", os.getenv("ALPACA_KEY"))
-print("ALPACA_SECRET:", os.getenv("ALPACA_SECRET"))
-print("BASE_URL:", os.getenv("BASE_URL"))
-print("SECRET_KEY:", os.getenv("SECRET_KEY"))
